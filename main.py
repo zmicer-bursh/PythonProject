e303c307660c6bb26fa34c46fe42b4f617c3db9b
zmicer-bursh <x0t1410@gmail.com>
2024-11-26 12:37:43 +0300

    test6

---
+++ main.py
@@ -0,0 +1,82 @@
+# print('Hello World')
+# command+alt+l выравнивание кода
+# command+/ все закоментить
+
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# # print('Hello', name, age)
+# print(f'Hello {name} {age}')
+
+# x = int(input('Enter first number: '))
+# y = int(input('Enter second number: '))
+# print('Sun:', x+y)
+# print(type('10'))
+
+# text = '10'
+# print(type(text))
+# print(type(int(text)))
+
+
+# print('I don\'t know english') #\-позваляет выводить апостоф или ""
+# print('Hello \n World') # перенос на новую строчку
+# print('Hello \t World') # табуляция большой пробел
+
+# age = 23
+# print(age)
+
+# is_ava верное название переменной, с цифр не называем, спецсимволы нет. Должна отражать смысл!
+# 2-4 слова максимум, важен регистр
+
+# MONTHS_IN_YEAR = 12  константа пишется в вехнем регистре всегда, называется польностью
+
+# типы данных
+# int число
+# float число с точкой
+# str - текст
+# bool - true/false
+
+# float_1 = 10.80000
+# float_2 = 10.000
+# float_3 = 10.
+#
+# print(f'float_1 = {int(float_1)} - {type(int(float_1))}')
+# print(type(float_2))
+# print(type(float_3))
+# print(float_2 + float_1)
+
+# print(5 / 2) деление всегда float
+
+# bool
+# is_active_1 = True
+# is_active_2 = False
+# # print(is_active_1)
+# # print(type(is_active_1))
+# print(int(is_active_1))
+
+# обращение по индексу
+# test_str = 'Hello world'
+# print(len(test_str)) #len длина строки
+# print(test_str[0]) #индекс символа с 0 начинается
+# print(test_str[3:10]) #срезы берем определенное кол-во символов
+# print(test_str[-3])
+# print(test_str[0:7:3]) #шаг
+
+# функции работы со строками
+# st = 'HELLO WorlD'
+# print(st.lower())
+# print(st.upper())
+# print(st.isdigit())
+# print(st.title())
+
+# print(st.find('l'))
+# print(st.rfind("l"))
+# print(st.replace("W", 'G').replace('r', ''))
+#
+# print(1 == 1) #True
+# print(1 == 2) #False
+# print(1 != 3) #True
+# print(1 != 1) #False
+# print(1 > 2) #False
+# print(1 < 4) #True
+# print(1 <= 2) #False
+# print(1 >= 4) #True
