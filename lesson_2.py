2e6a9180e233d5645d86c19644283437dbe545a9
zmicer-bursh <x0t1410@gmail.com>
2024-12-02 22:06:42 +0300

    lesson2

---
+++ lesson_2.py
@@ -0,0 +1,103 @@
+# a = 'text'
+# print(bool(a))
+#
+# b = ""
+# print(bool(b))
+#
+# number = 10
+# print(bool(number))
+#
+# number1 = 0
+# print(bool(number1))
+#
+# print(type(int("123")))
+
+# a = int(input())
+# b = int(input())
+#
+# summa = a + b
+# print(f"Summa: {summa}")
+# print(f"Summa: {a + b}") #лучше так
+
+# узнать айди переменной
+# a = 10
+# b = 10
+# c = a
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print(a)
+# print(b)
+
+# неизменяемые типы данных
+# int
+# float
+# str
+# bool
+# tuple () кортеж
+
+# изменяемые типы данных, создают новую ячейку памяти
+# list [] список
+# dict {} словарь
+# set {} множество
+
+# a = [1, 2, 3, [10, 20], True, {0: 1}]
+# print(a)
+# print(id(a))
+# a.append(10)
+# print(a)
+# print(id(a))
+
+# list_1 = [1, 2]
+# list_2 = []
+# list_2 = list_1
+# list_1.append(4)
+# print(id(list_1))
+# print(id(list_2))
+# print(list_2)
+#
+# tuple_1 = (1, 2)
+# tuple_2 = (1, 2)
+# print(id(tuple_1))
+# print(id(tuple_2))
+
+# set_1 = {1, 2, 4, 5, 5}
+# print(set_1)
+
+# dict_1 = {"Belarus": "Minsk","Poland": "Warsaw"}
+#
+#
+# print(dict_1)
+
+# индекс обращение
+# tuple_1 = (1, 2)
+# print(tuple_1[0])
+
+# list_1 = [1, 2]
+# del list_1[0]
+# b = list_1.pop(0)
+# print(list_1)
+# print(b)
+
+# foo = bool(12)
+# print(foo)
+
+# foo = str(12) #TODO: комментарий что нужно сделать
+# print(type(foo))
+
+# # идеальное написание функции
+# def summ(a: float, b: float) -> float:
+#     """
+#     Summa of 2 numbers
+#     :param a: first number
+#     :param b: second number
+#     return: summa of 2 numbers
+#     """
+#     return a + b
+
+import random
+
+
+
+
+
